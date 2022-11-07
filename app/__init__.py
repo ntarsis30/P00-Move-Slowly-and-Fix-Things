1746e920a0d9ff342a3335219d71c8940dbbf538
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-11-07 09:16:29 -0500

    started python

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+#Flying Turtles | Anson Wong, Nicholas Tarsis
+#SoftDev
+#P00 -- Move Slowly and Fix Things
+#2022-11-15
+#time spent: 
+from flask import Flask,session,request, redirect, url_for,render_template
+import os
+
+# Set the secret key to some random bytes. Keep this really secret!
+app = Flask(__name__)
+app.secret_key = os.urandom(32)

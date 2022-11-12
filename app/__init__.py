18703d265f74db20975c5b5228255a78fa4b1f4b
Anson <awong30@stuy.edu>
2022-11-12 00:58:07 -0500

    landing page- shows edited stories

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,26 @@
 #SoftDev
 #P00 -- Move Slowly and Fix Things
 #2022-11-15
-#time spent: 
+#time spent:
 from flask import Flask,session,request, redirect, url_for,render_template
 import os
+import sqlite3
 
 # Set the secret key to some random bytes. Keep this really secret!
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-app.route("\")
+@app.route('/landing', methods=['GET', 'POST'])
+def landing_page():
+    db = sqlite3.connect("database.db")
+    cur = db.cursor()
+    print("DIAG Tables")
+    print(cur.execute("SELECT name FROM sqlite_master").fetchall())
+    edited = cur.execute("SELECT * from edited_stories").fetchall()
+    print(edited)
+    db.close()
+    return render_template('landing.html', edited = edited)
+
 if __name__ == '__main__':
     app.debug = True
     app.run()

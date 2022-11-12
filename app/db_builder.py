7557e7350831be4f922e80136690676113ef3b4d
Anson <awong30@stuy.edu>
2022-11-12 00:55:37 -0500

    committed and closed db

--- app/db_builder.py
+++ app/db_builder.py
@@ -2,12 +2,12 @@
 #SoftDev
 #P00 -- Move Slowly and Fix Things
 #2022-11-15
-#time spent: 
+#time spent:
 from flask import Flask,session,request, redirect, url_for,render_template
 import sqlite3
 import os
 
-db = sqlite3.connect("database.db") 
+db = sqlite3.connect("database.db")
 c = db.cursor()
 
 if not """('logins',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
@@ -19,5 +19,8 @@ if not """('edited_stories',)""" in str((c.execute("SELECT name FROM sqlite_mast
 if not """('all_stories',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
     print("ga")
     c.execute("CREATE TABLE all_stories(username TEXT, story TEXT, newest TEXT)")
-
+#c.execute("""INSERT INTO edited_stories VALUES("test title", "Test story") """)
+#c.execute("DELETE FROM edited_stories")
 print((c.execute("SELECT name FROM sqlite_master")).fetchall())
+db.commit()
+db.close()

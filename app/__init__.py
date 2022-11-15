0ad207e6933c4369c01357a97a16f72ce9b3b86e
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-11-14 23:03:02 -0500

    added login functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,48 @@
 #P00 -- Move Slowly and Fix Things
 #2022-11-15
 #time spent:
-from flask import Flask,session,request, redirect, url_for,render_template
+from flask import *
 import os
 import sqlite3
+import db_builder
 
 # Set the secret key to some random bytes. Keep this really secret!
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return redirect("/home")
+    return render_template('login.html')
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if db_builder.verify(username,password):
+        session['username'] = username
+        session['password'] = password
+        return redirect("/home")
+    if request.form.get('submit_button') != None:
+        return render_template("create_account.html")
+    response = make_response(render_template('error.html',message = "credentials are not correct"))
+    return response
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        user_info = request.form.get('username')
+        pass_info = request.form.get('password') 
+        if db_builder.account_used(user_info):
+            return "account with this username already exists"
+        else:
+            return render_template("signed_in.html")
+    return redirect(url_for('index'))
+    
+
+"""
 @app.route('/landing', methods=['GET', 'POST'])
 def landing_page():
     db = sqlite3.connect("database.db")
@@ -21,6 +55,7 @@ def landing_page():
     print(edited)
     db.close()
     return render_template('landing.html', edited = edited)
+"""
 
 if __name__ == '__main__':
     app.debug = True
--- app/db_builder.py
+++ app/db_builder.py
@@ -3,24 +3,74 @@
 #P00 -- Move Slowly and Fix Things
 #2022-11-15
 #time spent:
-from flask import Flask,session,request, redirect, url_for,render_template
+from flask import *
 import sqlite3
 import os
 
 db = sqlite3.connect("database.db")
 c = db.cursor()
+stories = "(title TEXT, story TEXT, last TEXT, editors TEXT)"
+users = ("(username TEXT, password TEXT)")
 
-if not """('logins',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
-    print("g")
-    c.execute("CREATE TABLE logins(username TEXT, password TEXT)")
-if not """('edited_stories',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
-    print("ge")
-    c.execute("CREATE TABLE edited_stories(title TEXT, story TEXT)")
-if not """('all_stories',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
-    print("ga")
-    c.execute("CREATE TABLE all_stories(username TEXT, story TEXT, newest TEXT)")
+def data_query(table, info = None):
+    db = sqlite3.connect("database.db", check_same_thread=False)
+    c = db.cursor()
+    if info == None:
+        output = c.execute(table)
+    else:
+        output = c.execute(table, extra)
+    db.commit()
+    db.close()
+    return output
+def make_table(name, outline):
+    data_query(f"CREATE TABLE IF NOT EXISTS {name} {outline}")
+
+make_table("Story", stories)
+make_table("User", users)
+
+def get_table_list(name):
+    db = sqlite3.connect("database.db", check_same_thread=False)
+    c = db.cursor()
+    curr = c.execute(f"SELECT * from {name}")
+    out = curr.fetchall()
+    db.commit()
+    db.close()
+    return out
+
+def verify(username, password):
+    accounts = get_table_list("User")
+    for account in accounts:
+        if account[0] == username and account[1] == password:
+            return True
+    return False
+
+def account_used(username):
+    accounts = get_table_list("User")
+    for account in accounts:
+        if account[0] == username:
+            return True
+    return False
+
+def add_account(username, password):
+    if not(account_exists(username)):
+        query("INSERT INTO User VALUES (?, ?)", (username, password))
+    else:
+        return -1
+
+
+
+#if not """('logins',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
+#    print("g")
+#    c.execute("CREATE TABLE logins(username TEXT, password TEXT)")
+#if not """('edited_stories',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
+#    print("ge")
+#    c.execute("CREATE TABLE edited_stories(title TEXT, story TEXT)")
+#if not """('all_stories',)""" in str((c.execute("SELECT name FROM sqlite_master")).fetchall()):
+#    print("ga")
+#    c.execute("CREATE TABLE all_stories(username TEXT, story TEXT, newest TEXT)")
 #c.execute("""INSERT INTO edited_stories VALUES("test title", "Test story") """)
 #c.execute("DELETE FROM edited_stories")
-print((c.execute("SELECT name FROM sqlite_master")).fetchall())
+#print((c.execute("SELECT name FROM sqlite_master")).fetchall())
+
 db.commit()
 db.close()
